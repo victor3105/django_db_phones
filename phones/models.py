b77fb6f8341d464c21b375b34768a754c3d720b0
Victor Glebov <glebovv3105@gmail.com>
2020-10-13 21:13:55 -0600

    Added Phone model

--- phones/models.py
+++ phones/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=40)
+    price = models.DecimalField(max_length=6)
+    image = models.CharField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        # Check if it is a newly created object
+        if not self.id:
+            self.slug = slugify(self.name)
+
+        super(Phone, self).save(*args, **kwargs)
