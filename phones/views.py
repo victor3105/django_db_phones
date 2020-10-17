0f7a11d93443cf77cc8ccc01eb8597491d817de4
Victor Glebov <glebovv3105@gmail.com>
2020-10-16 22:27:31 -0600

    Added sorting

--- phones/views.py
+++ phones/views.py
@@ -5,13 +5,25 @@ from phones.models import Phone
 
 def show_catalog(request):
     template = 'catalog.html'
+    sort = request.GET.get('sort')
+    # Default behavior is to show phone in the order
+    # in which they are stored in the database
     phones = Phone.objects.all()
+    # Sort phones
+    if sort == 'name':
+        phones = Phone.objects.order_by('name')
+    elif sort == 'min_price':
+        phones = Phone.objects.order_by('price')
+    elif sort == 'max_price':
+        phones = Phone.objects.order_by('-price')
     phone_list = []
     for phone in phones:
         phone_info = {'Name': phone.name, 'Price': phone.price,
                       'Image': phone.image, 'URL': f'{reverse("catalog")}{phone.slug}'}
         phone_list.append(phone_info)
-    context = {'phones': phone_list}
+    # For sorting
+    catalog_url = reverse('catalog').rstrip('/')
+    context = {'phones': phone_list, 'catalog_url': catalog_url}
     return render(request, template, context)
 
 
